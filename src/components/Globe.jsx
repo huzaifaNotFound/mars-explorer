c3f386c47ebcb8ace60a788e5c357ffde0bf1af2
huzaifa <idkmyname9009@gmail.com>
2026-09-22 21:43:18 +0530

    globe texture

--- src/components/Globe.jsx
+++ src/components/Globe.jsx
@@ -1,20 +1,41 @@
+import { useEffect, useState } from "react";
 import Globe from "react-globe.gl";
+import * as THREE from "three";
 
-export default function MarsGlobe({ terrainEnabled = true }) {
+export default function MarsGlobe() {
+  const [material, setMaterial] = useState(null);
+
+  useEffect(() => {
+    const loader = new THREE.TextureLoader();
+    
+    const colorMap = loader.load('/textures/mars-color.jpg');
+    colorMap.colorSpace = THREE.SRGBColorSpace; 
+    const displacementMap = loader.load('/textures/texture-mars.jpg');
+    
+    const customMaterial = new THREE.MeshPhongMaterial({
+      map: colorMap,
+      bumpMap: displacementMap,
+      bumpScale: 0.015,
+      displacementMap: displacementMap,
+      displacementScale: 3, 
+    });
+
+    setMaterial(customMaterial);
+  }, []);
+
   return (
     <div className="w-full h-full">
-      <Globe
-        width={window.innerWidth}
-        height={window.innerHeight}
-        globeImageUrl="/textures/mars-color.jpg"
-        bumpImageUrl={
-          terrainEnabled ? "/textures/mars-bump.jpeg" : null
-        }
-        bumpScale={0.15}
-        backgroundColor="#05070a"
-        animateIn={true}
-        enablePointerInteraction={true}
-      />
-    </div>
+      {material && (
+        <Globe
+          width={window.innerWidth}
+          height={window.innerHeight}
+          globeMaterial={material}
+          backgroundColor="#05070a"
+          animateIn={true}
+          enablePointerInteraction={true}
+          showAtmosphere={false}
+        />
+      )}
+    </div> 
   );
 }
